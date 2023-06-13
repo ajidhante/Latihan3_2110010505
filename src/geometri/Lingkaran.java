3cc455d7cab238bc9f3dc762775f172ec6407ddd
ajidhante <babung123456789@gmail.com>
2023-06-13 09:06:33 +0800

    Membuat Class Lingkaran dan PersegiPanjang

---
+++ src/geometri/Lingkaran.java
@@ -0,0 +1,17 @@
+package geometri;
+
+public class Lingkaran {
+
+    //rumus
+    double phi = 3.14;
+    int r;
+    double L = phi*(r*r);
+    double K = 2*phi*r;
+    
+    r
+    
+    L = phi*(r*r);
+    K = 2*phi*r;
+    
+    
+}
